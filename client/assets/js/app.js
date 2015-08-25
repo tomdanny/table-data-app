681b9d1ff845f4dc9aadc0a970c61a9c528c0299
Daniel Tomic <tom_danny2003@yahoo.com>
2015-08-25 17:13:28 -0400

    Working foundation for apps modal form

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -34,22 +34,61 @@
     FastClick.attach(document.body);
   }
 
+  // START CUSTOM APP CODE
 
+  // Start custom controller 
 
- angular.module('application').controller('formatCtrl', ['$scope', function (scope) {
-    scope.rowCollection = [
-        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
-        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
-    ];
+//   angular.module('application').controller('formatCtrl', ['$scope', function (scope) {
 
-    scope.removeRow = function removeRow(row) {
-        var index = scope.rowCollection.indexOf(row);
-        if (index !== -1) {
-            scope.rowCollection.splice(index, 1);
-        }
+//     scope.removeRow = function removeRow(row) {
+//         var index = scope.rowCollection.indexOf(row);
+//         if (index !== -1) {
+//             scope.rowCollection.splice(index, 1);
+//         }
+//     }
+// }]);
+
+  angular.module('application').controller('formatCtrl', formatCtrl);
+
+  formatCtrl.$inject = ['$scope', '$stateParams', '$state', '$controller'];
+
+    function formatCtrl($scope, $stateParams, $state, $controller) {
+
+      angular.extend(this, $controller('DefaultController', {$scope: $scope, $stateParams: $stateParams, $state: $state}));
+
+      $scope.editMode = false;
+
+      $scope.showstatus = 'incomplete';
+
+      $scope.showtag = {};
+
+      $scope.rows = JSON.parse(localStorage.getItem("saved_tasks"));
+      if (typeof $scope.rows === 'undefined' || $scope.rows === null){
+      $scope.rows = [];
+      }
+
+      $scope.CreatePerson = function(firstName, lastName, role, date, email){
+      var row = {};
+
+      row.firstName = firstName;
+      row.lastName = lastName;
+      row.role = role; 
+      row.date = new Date();
+      row.email = email;
+      row.id = Date.now();
+
+      $scope.rows.push(row);
+
+      //localStorage.setItem("all_tags", JSON.stringify($scope.allTags));
+      localStorage.setItem("saved_tasks", JSON.stringify($scope.rows));
+      };
+
     }
-}]);
+
+  // End custom controller
+
+  // END CUSTOM APP CODE
+
 
 })();
 
